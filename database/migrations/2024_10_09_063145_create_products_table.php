85289c8668d72828c6e327faeb346f5d06460434
asfak007 <sajalasfakrobin@gmail.com>
2024-10-09 12:52:53 +0600

    admin models

---
+++ database/migrations/2024_10_09_063145_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->integer('quantity');
+            $table->integer('price');
+            $table->longText('thumbnail');
+            $table->string('first_image');
+            $table->string('second_image');
+            $table->string('third_image');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
