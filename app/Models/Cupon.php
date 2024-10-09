c50c43a1f3576bbcead5a9694b7981df5399a00f
asfak007 <sajalasfakrobin@gmail.com>
2024-10-09 13:23:46 +0600

    confing coupon model

--- app/Models/Cupon.php
+++ app/Models/Cupon.php
@@ -2,11 +2,26 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Cupon extends Model
 {
     use HasFactory;
     protected $fillable = ['name','discount','valid-until'];
+    /**
+    convert the coupon name to upperCaser
+    */
+    public function setNameAttribute($value){
+        $this->attributes['name'] = Str::upper($value);
+    }
+    public function checkIfValid(){
+        if($this->valid_until > Carbon::now()){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
--- app/Models/order.php
+++ app/Models/order.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class order extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['qty', 'total', 'delivered_at', 'user_id', 'coupon_id'];
+
+    public function products()
+    {
+        return $this->belongsToMany(product::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
